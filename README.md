29eaf90e0fa0f1b0a56bb8109526bbc917a69c9a
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-12-01 23:19:07 -0500

    😔Last try on this project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# YouTube-Suscriber-Bot
+# YouTube-Suscriber-Bot `[DEPRECATED]`
 
 The idea is to create a bot that subscribes to a given channel automatically
 
@@ -11,3 +11,11 @@ python -m venv venv
 ./venv/Scripts/activate
 pip intall -r requirements.txt
 ```
+
+## What I meant by `[DEPRECATED]`?
+
+It means that I won't longer support this project, this due that the project didn't work fully,
+the project project works fine sometimes, but sometimes it crashes.
+
+So, if someone can use it as a base for future projects, or it's capable to make the project work
+100% fine, well... Go ahead, I will be willing to learn how you did it.
--- youtube_subscriber.py
+++ youtube_subscriber.py
@@ -1,11 +1,14 @@
 import os
 import csv
+import time
 from os.path import join, dirname
 from dotenv import load_dotenv
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
+import undetected_chromedriver.v2 as uc
 
 # * Global variables
 dotenv_path = join(dirname(__file__), '.env')
@@ -13,14 +16,20 @@ csv_path = join(dirname(__file__), 'accounts.csv')
 load_dotenv(dotenv_path)
 accounts = []
 
+subscribe_button_xpath = '/html/body/ytd-app/ytd-popup-container/tp-yt-paper-dialog/yt-confirm-dialog-renderer/div[2]/div/yt-button-renderer[2]/a/tp-yt-paper-button'
+
 URL = os.environ.get('URL')
-# driver = webdriver.Chrome()
+# chrome_options = Options()
+# chrome_options.add_argument("--disable-extensions")
+# chrome_options.add_argument("--disable-web-security")
+# chrome_options.add_argument("--allow-running-insecure-content")
+# driver = webdriver.Chrome(chrome_options=chrome_options)
 
 
-# def click_element_by_xpath(path):
-#     element = WebDriverWait(driver, 10).until(
-#         lambda x: x.find_element_by_xpath(path))
-#     element.click()
+def click_element_by_xpath(path):
+    element = WebDriverWait(driver, 10).until(
+        lambda x: x.find_element_by_xpath(path))
+    element.click()
 
 
 def print_csv_file(csv_path):
@@ -37,7 +46,37 @@ def load_csv_as_dict(csv_path):
             accounts.append((row['account'], row['password']))
 
 
+def subscribe_to_the_channel(email, password):
+    driver.get(URL)
+    # time.sleep(5)
+    click_element_by_xpath(subscribe_button_xpath)
+
+    time.sleep(3)
+    google_email_form = driver.find_element_by_xpath(
+        '/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div/div[1]/div/div[1]/input')
+
+    google_email_form.send_keys(email)
+    google_email_form.send_keys(Keys.ENTER)
+
+    time.sleep(3)
+    google_password_form = driver.find_element_by_xpath(
+        '/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input')
+
+    google_password_form.send_keys(password)
+    google_password_form.send_keys(Keys.ENTER)
+
+    time.sleep(3)
+    click_element_by_xpath(
+        '/html/body/ytd-app/ytd-popup-container/tp-yt-paper-dialog/yt-confirm-dialog-renderer/div[2]/div/yt-button-renderer[2]/a/tp-yt-paper-button/yt-formatted-string')
+
+
 if __name__ == "__main__":
     print(URL)
     load_csv_as_dict(csv_path)
-    print(accounts[0])
+
+    for email, password in accounts:
+        driver = uc.Chrome(
+            executable_path="C:\\WebDriver\\bin\\chromedriver.exe")
+        subscribe_to_the_channel(email, password)
+        driver.close()
+        driver.quit()
